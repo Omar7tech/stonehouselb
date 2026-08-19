2a1e2bab0a90e4b702ef73916abd8356b41276aa
Omar Abi Farraj <omar.7tech@gmail.com>
2026-08-19 13:04:54 +0300

    favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,22 +1,28 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        @fonts
+    <link rel="icon" type="image/png" href="/favicon-96x96.png?v=20260819" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg?v=20260819" />
+    <link rel="shortcut icon" href="/favicon.ico?v=20260819" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=20260819" />
+    <meta name="apple-mobile-web-app-title" content="Stone House" />
+    <link rel="manifest" href="/site.webmanifest?v=20260819" />
 
-        @viteReactRefresh
-        @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
-        <x-inertia::head>
-            <title>{{ config('app.name', 'Laravel') }}</title>
-        </x-inertia::head>
-    </head>
-    <body class="font-sans antialiased">
-        <x-inertia::app />
-    </body>
+    @fonts
+
+    @viteReactRefresh
+    @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
+    <x-inertia::head>
+        <title>{{ config('app.name', 'Laravel') }}</title>
+    </x-inertia::head>
+</head>
+
+<body class="font-sans antialiased">
+    <x-inertia::app />
+</body>
+
 </html>
